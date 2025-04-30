a5e0567219a9c6b5de9f49dc023ac856b008b67b
kacperut <kacper.rutkowski@idemia.com>
2025-04-30 15:42:54 +0200

    update on inputs.nixpkgs.url

--- flake.nix
+++ flake.nix
@@ -1,8 +1,10 @@
 {
+
   description = "My Home Manager configuration";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
