efca9cf77a82f6fca2794d997efa51236ab4dc37
kacperut <kacper.rutkowski@idemia.com>
2025-05-07 15:37:45 +0200

    kitty desktop wip

--- home/modules/common/kitty.nix
+++ home/modules/common/kitty.nix
@@ -1,10 +1,13 @@
 
-{ config, pkgs, ... }:
-
+{ config, pkgs, lib, ... }:
+let
+  localApplications = ".local/share/applications";
+  kittyPkg = config.lib.nixGL.wrap pkgs.kitty;
+in
 {
   programs.kitty = {
     enable = true;
-    package = config.lib.nixGL.wrap pkgs.kitty;
+    package = kittyPkg;
     themeFile = "Catppuccin-Mocha";
     shellIntegration.enableZshIntegration = true;
     font = {
@@ -24,4 +27,15 @@
   home.file.".config/kitty/launch.conf".text = ''
   launch sh -c "tmux -T hyperlinks new -A -s 0"
 '';
+
+  # Copy .desktop files from the Nix store to the applications directory
+  home.file."${localApplications}/kitty.desktop".source = "${kittyPkg}/share/applications/kitty.desktop";
+  home.file."${localApplications}/kitty-open.desktop".source = "${kittyPkg}/share/applications/kitty-open.desktop";
+  home.file.".local/bin/kitty".source = "${kittyPkg}/bin/kitty";
+  home.file.".local/bin/kitten".source = "${kittyPkg}/bin/kitten";
+  # Patch the .desktop files after they're created, using the variable
+  home.activation.fixKittyDesktop = lib.hm.dag.entryAfter ["linkGeneration"] ''
+    sed -i "s|Exec=kitty|Exec=${kittyPkg}/bin/kitty|g" ${localApplications}/kitty*.desktop
+    sed -i "s|Icon=kitty|Icon=${kittyPkg}/share/icons/hicolor/256x256/apps/kitty.png|g" ${localApplications}/kitty*.desktop
+  '';
 }
