a5a897d33f1c6aa54ff3b58125875dd1436c80b1
Ekajto <kac.rutkowski@gmail.com>
2025-05-11 21:55:49 +0200

    adjusted config for lua in nvim

--- home/modules/common/nvim.nix
+++ home/modules/common/nvim.nix
@@ -117,5 +117,5 @@
     "${parsers}/parser";
 
   # Normal LazyVim config here, see https://github.com/LazyVim/starter/tree/main/lua
-  xdg.configFile."nvim/lua".source = ./lua;
+  xdg.configFile."nvim/lua".source = ./config/lua;
 }
